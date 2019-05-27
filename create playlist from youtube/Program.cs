a58e27a21cc27d19ca9d8a330573cf71ddf0ba9e
Lyuboslav Tihomirov Lyubenov <lubidead1@mail.bg>
2019-05-27 13:51:40 +0300

    Improved song search "algorithm"

--- create playlist from youtube/Program.cs
+++ create playlist from youtube/Program.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace create_playlist_from_youtube
 {
@@ -32,7 +33,7 @@ namespace create_playlist_from_youtube
                     AccessToken = token.AccessToken
                 };
 
-                var playlist = api.GetPlaylist(auth.ClientId, playlistId);
+                var playlist = api.GetPlaylist(playlistId);
                 
                 if (playlist.HasError())
                 {
@@ -44,20 +45,23 @@ namespace create_playlist_from_youtube
                 
                 foreach (var videoName in videosNames)
                 {
+                
+                    var songName = videoName.ToLower().Replace("-", "");
+                    songName = Regex.Replace(songName, @"[(\[][A-Za-z0-9 ]+[)\]]", "");
                     Console.WriteLine("Searching for " + videoName);
 
-                    var cuttedVideoName = videoName.Substring(0, Math.Min(40, videoName.Length));
+                    var cuttedVideoName = songName.Substring(0, Math.Min(35, songName.Length));
                     var searchResult = api.SearchItems(cuttedVideoName, SpotifyAPI.Web.Enums.SearchType.Track, 1);
                     if (searchResult.HasError() || searchResult.Tracks == null || searchResult.Tracks.Items == null ||!searchResult.Tracks.Items.Any())
                     {
-                        Console.WriteLine("Cant find song matching " + videoName);
+                        Console.WriteLine("Cant find song matching " + songName);
                         continue;
                     }
 
                     var track = searchResult.Tracks.Items.First();
                     var trackUri = track.Uri;
 
-                    var addResult = api.AddPlaylistTrack(auth.ClientId, playlist.Id, trackUri);
+                    var addResult = api.AddPlaylistTrack(playlist.Id, trackUri);
 
                     if (addResult.HasError())
                     {
